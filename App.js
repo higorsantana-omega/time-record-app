5c1b00e25cfafe5ddf1a1670c9c5a9e0372fae68
higorsantana-omega <omegadosgames02@gmail.com>
2022-10-12 01:30:33 -0300

    feat: default font

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import "./nativewind-output.js"
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from "@expo-google-fonts/inter";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
+  if (!fontsLoaded) return null
+
   return (
     <View className="flex-1 items-center justify-center ">
       <Text className="text-violet-600 text-5xl">Open up App.js to start working on your app!</Text>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ const nativewind = require("nativewind/tailwind/native")
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    },
   },
   plugins: [nativewind()]
 }
